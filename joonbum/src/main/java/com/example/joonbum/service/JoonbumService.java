cfcfff12338395c1493d1c40414b50548fb42e04
joonbum <1996joon@gmail.com>
2023-11-30 14:41:46 +0900

    .

--- joonbum/src/main/java/com/example/joonbum/service/JoonbumService.java
+++ joonbum/src/main/java/com/example/joonbum/service/JoonbumService.java
@@ -2,7 +2,9 @@ package com.example.joonbum.service;
 
 import com.example.joonbum.api.JoonbumRibbonTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JoonbumService {
     @Autowired
     private JoonbumRibbonTemplate joonbumRibbonTemplate;
