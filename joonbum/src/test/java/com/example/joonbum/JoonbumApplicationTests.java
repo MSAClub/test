7237f2ea28643972dc1f3d6c4d72517b6483e3e8
joonbum <1996joon@gmail.com>
2023-11-30 14:02:19 +0900

    .

---
+++ joonbum/src/test/java/com/example/joonbum/JoonbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joonbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
