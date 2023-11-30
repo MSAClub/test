afce5b98c8d2125bc77273aa08b00a35c4733562
ggramgyo <woghk6761@pusan.ac.kr>
2023-11-30 13:59:11 +0900

    feat: add jaehwa clinet

---
+++ jaehwa/src/main/java/com/example/jaehwa/JaehwaApplication.java
@@ -0,0 +1,15 @@
+package com.example.jaehwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JaehwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaehwaApplication.class, args);
+	}
+
+}
---
+++ jaehwa/src/test/java/com/example/jaehwa/JaehwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaehwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaehwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
