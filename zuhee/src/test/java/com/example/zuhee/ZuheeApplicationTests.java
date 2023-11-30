63c2efe355f38a2347a314ddf8fa089580aad74e
zoohee <jjhh1545@gmail.com>
2023-11-30 13:58:32 +0900

    zuhee test

---
+++ zuhee/src/test/java/com/example/zuhee/ZuheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuhee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
