d78dc6da12592fcaebd5f903105bd185d19c7e10
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-12-20 19:43:28 +0900

    chore : api-gateway-test

--- dongjae/src/main/java/com/example/dongjae/controller/DongJaeController.java
+++ dongjae/src/main/java/com/example/dongjae/controller/DongJaeController.java
@@ -2,12 +2,12 @@ package com.example.dongjae.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/dongjae")
 public class DongJaeController {
-//    @Autowired
-//    DongJaeService dongJaeService;
 
     @GetMapping("/test/{id}")
     public String getTest(@PathVariable String id) {
